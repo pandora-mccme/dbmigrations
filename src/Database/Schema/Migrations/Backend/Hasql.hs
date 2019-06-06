4a679f3e54f5cd1a990a8151b95fcb9861861477
artik008 <artik008@yandex.ru>
2019-06-06 15:40:41 +0300

    Update to LTS-13.24

--- src/Database/Schema/Migrations/Backend/Hasql.hs
+++ src/Database/Schema/Migrations/Backend/Hasql.hs
@@ -4,25 +4,22 @@ module Database.Schema.Migrations.Backend.Hasql
     )
 where
 
-import Hasql.Session
-import Hasql.Query (statement)
-import qualified Hasql.Encoders as H.Encode
-import Hasql.Decoders
-import Hasql.Connection
+import           Hasql.Connection
+import           Hasql.Decoders
+import qualified Hasql.Encoders                       as H.Encode
+import           Hasql.Session
+import           Hasql.Statement
 
-import Database.Schema.Migrations.Backend
-    ( Backend(..)
-    , rootMigrationName
-    )
-import Database.Schema.Migrations.Migration
-    ( Migration(..)
-    , newMigration
-    )
+import           Database.Schema.Migrations.Backend   (Backend (..),
+                                                       rootMigrationName)
+import           Database.Schema.Migrations.Migration (Migration (..),
+                                                       newMigration)
 
-import Data.ByteString (ByteString)
-import Data.Monoid ( (<>) )
-import Data.Time.Clock (getCurrentTime)
-import System.Exit (ExitCode (ExitFailure), exitWith)
+import           Data.ByteString                      (ByteString)
+import           Data.Monoid                          ((<>))
+import           Data.Time.Clock                      (getCurrentTime)
+import           System.Exit                          (ExitCode (ExitFailure),
+                                                       exitWith)
 
 migrationTableName :: ByteString
 migrationTableName = "installed_migrations"
@@ -37,9 +34,18 @@ revertSql = "DROP TABLE " <> migrationTableName
 hasqlBackend :: Connection -> Backend
 hasqlBackend conn =
     Backend { isBootstrapped = do
-                exists <- run (query () (statement "SELECT migration_id FROM installed_migrations WHERE FALSE" H.Encode.unit unit True)) conn
+                exists <- run (
+                  statement
+                    ()
+                    (Statement
+                      "SELECT migration_id FROM installed_migrations WHERE FALSE"
+                      H.Encode.unit
+                      unit
+                      True
+                    )
+                  ) conn
                 return $ case exists of
-                  Left _ -> False
+                  Left _  -> False
                   Right _ -> True
             , getBootstrapMigration = do
                 ts <- getCurrentTime
@@ -53,12 +59,12 @@ hasqlBackend conn =
             , applyMigration = \m -> do
                 reportAction <- run (sql $ mApply m) conn
                 case reportAction of
-                  Left e -> reportSqlError e
+                  Left e  -> reportSqlError e
                   Right i -> return i
                 register <- run (sql $ "INSERT INTO " <> migrationTableName <>
                           " (migration_id) VALUES ('" <> mId m <> "')") conn
                 case register of
-                  Left e -> reportSqlError e
+                  Left e  -> reportSqlError e
                   Right i -> return i
                 return ()
 
@@ -68,27 +74,36 @@ hasqlBackend conn =
                     Just revQ -> do
                       action <- run (sql revQ) conn
                       case action of
-                        Left e -> reportSqlError e
+                        Left e  -> reportSqlError e
                         Right i -> return i
                   -- Remove migration from installed_migrations in either case.
                   deleteAction <- run (sql $ "DELETE FROM " <> migrationTableName <>
                             " WHERE migration_id = '" <> mId m <> "'") conn
                   case deleteAction of
-                    Left e -> reportSqlError e
+                    Left e  -> reportSqlError e
                     Right i -> return i
                   return ()
 
             , getMigrations = do
-                selectNames <- run (query () (statement "SELECT migration_id FROM installed_migrations" H.Encode.unit (rowsList $ value bytea) False)) conn
+                selectNames <- run (
+                  statement
+                    ()
+                    (Statement
+                      "SELECT migration_id FROM installed_migrations"
+                      H.Encode.unit
+                      (rowList $ column bytea)
+                      False
+                    )
+                  ) conn
                 results <- case selectNames of
-                  Left e -> reportSqlError e
+                  Left e      -> reportSqlError e
                   Right names -> return names
                 return results
 
             , disconnectBackend = release conn
             }
 
-reportSqlError :: Error -> IO a
+reportSqlError :: QueryError -> IO a
 reportSqlError e = do
   putStrLn $ "\n" <> "A database error occurred: " <> show e
   exitWith (ExitFailure 1)
